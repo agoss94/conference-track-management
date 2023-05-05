92df395332f2eefb21615ae7467bcf5e62649d91
Andreas <andreas.goss1994@gmail.com>
2023-05-05 22:05:41 +0200

    Convert tabs to spaces

--- src/main/java/com/github/agoss94/track/manager/Event.java
+++ src/main/java/com/github/agoss94/track/manager/Event.java
@@ -7,41 +7,41 @@ import java.time.Duration;
  */
 public class Event {
 
-	/**
-	 * The title of the event.
-	 */
-	private final String title;
+    /**
+     * The title of the event.
+     */
+    private final String title;
 
-	/**
-	 * The duration of the talk in minutes.
-	 */
-	private final Duration duration;
+    /**
+     * The duration of the talk in minutes.
+     */
+    private final Duration duration;
 
-	/**
-	 * Simple constructor creates a Talk object with the given title and specified
-	 * duration.
-	 *
-	 * @param title    the title of the talk.
-	 * @param duration the duration of the talk. The duration may be {@code null},
-	 *                 which signals that the event is open ended.
-	 */
-	public Event(String title, Duration duration) {
-		this.title = title;
-		this.duration = duration;
-	}
+    /**
+     * Simple constructor creates a Talk object with the given title and specified
+     * duration.
+     *
+     * @param title    the title of the talk.
+     * @param duration the duration of the talk. The duration may be {@code null},
+     *                 which signals that the event is open ended.
+     */
+    public Event(String title, Duration duration) {
+        this.title = title;
+        this.duration = duration;
+    }
 
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return title;
-	}
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
 
-	/**
-	 * @return the duration
-	 */
-	public Duration getDuration() {
-		return duration;
-	}
+    /**
+     * @return the duration
+     */
+    public Duration getDuration() {
+        return duration;
+    }
 
 }
