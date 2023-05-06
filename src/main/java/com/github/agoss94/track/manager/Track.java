848fe1d7fe64ff8c5a0d128452c1a02533a1d0e0
Andreas <andreas.goss1994@gmail.com>
2023-05-06 09:58:24 +0200

    Add Track class.

---
+++ src/main/java/com/github/agoss94/track/manager/Track.java
@@ -0,0 +1,9 @@
+package com.github.agoss94.track.manager;
+
+/**
+ * A track is a timetable of non-overlapping events. As such a track maps points
+ * in time to events.
+ */
+public class Track {
+
+}
